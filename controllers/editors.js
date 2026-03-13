840b92b0e3a9212ec9001d2a7f026e2faafa70ab
Haobo Wang <wangh518@mcmaster.ca>
2026-03-13 11:51:08 -0400

    Display users and articles tables on editor dashboard

--- controllers/editors.js
+++ controllers/editors.js
@@ -1,10 +1,22 @@
-const express = require('express');
-var router = express.Router()
+const express = require("express");
+const router = express.Router();
+const usersModel = require("../models/users");
+const articlesModel = require("../models/articles");
 
-// Display the editors page
-router.get("/", async function(req, res)
-{
-  res.render("editors", req.TPL);
+router.get("/", async function(req, res) {
+  try {
+    const users = await usersModel.getAllUsers();
+    const articles = await articlesModel.getAllArticles();
+
+    req.TPL.users = users;
+    req.TPL.articles = articles;
+
+    res.render("editors", req.TPL);
+  } catch (err) {
+    console.log(err);
+    req.TPL.error = "Error loading editor dashboard.";
+    res.render("editors", req.TPL);
+  }
 });
 
 module.exports = router;
